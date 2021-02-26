dd1b7819df4068b5b812d80ec6c4376cdbb9cca3
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-02-26 22:41:45 +0200

    add last additional problem

---
+++ Inheritance Exercise/Animals/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name,int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public virtual string ProduceSound()
+        {
+            return string.Empty;
+        }
+        public override string ToString()
+        {
+            return $"{Name} {Age} {Gender}";
+        }
+    }
+}
--- Inheritance Exercise/Animals/StartUp.cs
+++ Inheritance Exercise/Animals/StartUp.cs
@@ -1,10 +1,66 @@
-﻿namespace Animals
+﻿using System;
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            string command = Console.ReadLine();
+            string[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (true)
+            {
+                if (int.Parse(data[1]) <= 0 || data[0] == null || data[2] == null)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                if (command == "Cat")
+                {
+                    Cat cat = new Cat(data[0], int.Parse(data[1]), data[2]);
+                    Console.WriteLine(cat.GetType().Name);
+                    Console.WriteLine(cat);
+                    Console.WriteLine(cat.ProduceSound());
+                }
+                else if(command == "Dog")
+                {
+                    Dog dog = new Dog(data[0], int.Parse(data[1]), data[2]);
+                    Console.WriteLine(dog.GetType().Name);
+                    Console.WriteLine(dog);
+                    Console.WriteLine(dog.ProduceSound());
+                }
+                else if(command == "Frog")
+                {
+                    Frog frog = new Frog(data[0], int.Parse(data[1]), data[2]);
+                    Console.WriteLine(frog.GetType().Name);
+                    Console.WriteLine(frog);
+                    Console.WriteLine(frog.ProduceSound());
+                }
+                else if(command == "Kitten")
+                {
+                    Kitten kitten = new Kitten(data[0], int.Parse(data[1]));
+                    Console.WriteLine(kitten.GetType().Name);
+                    Console.WriteLine(kitten);
+                    Console.WriteLine(kitten.ProduceSound());
+                }
+                else if(command == "Tomcat")
+                {
+                    Tomcat tomcat = new Tomcat(data[0], int.Parse(data[1]));
+                    Console.WriteLine(tomcat.GetType().Name);
+                    Console.WriteLine(tomcat);
+                    Console.WriteLine(tomcat.ProduceSound());
+                }
+
+
+                command = Console.ReadLine();
+                if (command == "Beast!")
+                {
+                    break;
+                }
+                data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+        
+
         }
     }
 }
