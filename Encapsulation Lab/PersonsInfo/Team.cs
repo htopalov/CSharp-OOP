612998fe232c72dec23f16bf4f0991d68a303765
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-02-26 22:24:17 +0200

    four problems into one=>from lecture

---
+++ Encapsulation Lab/PersonsInfo/Team.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+            this.name = name;
+        }
+
+        public IReadOnlyList<Person> FirstTeam 
+        {
+            get => firstTeam.AsReadOnly();
+        }
+        public IReadOnlyList<Person> ReserveTeam 
+        {
+            get => reserveTeam.AsReadOnly();
+        }
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
