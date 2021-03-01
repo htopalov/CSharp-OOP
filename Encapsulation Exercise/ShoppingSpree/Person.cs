9f1fd08c81bb99b206e936b380c754c566fdc933
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-03-01 12:20:51 +0200

    cool problem

---
+++ Encapsulation Exercise/ShoppingSpree/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bag;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bag = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            { return this.name; }
+            private set
+            {
+                if (value == string.Empty || value == " ")
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                else
+                {
+                    this.name = value;
+                }
+            }
+        }
+        public decimal Money
+        {
+            get { return this.money; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                else
+                {
+                    this.money = value;
+                }
+            }
+        }
+        public IReadOnlyList<Product> ProductList
+        {
+            get => bag.AsReadOnly();
+        }
+        public void Shop(Product product)
+        {
+            if (this.Money >= product.Cost)
+            {
+                this.Money -= product.Cost;
+                bag.Add(product);
+                Console.WriteLine($"{this.Name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name} can't afford {product.Name}");
+            }
+        }
+    }
+}
---
+++ Encapsulation Exercise/ShoppingSpree/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Person> peopleList = new List<Person>();
+                List<Product> productsList = new List<Product>();
+                string[] people = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+                string[] products = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < people.Length; i++)
+                {
+                    string[] currentPerson = people[i].Split("=", StringSplitOptions.RemoveEmptyEntries);
+                    peopleList.Add(new Person(currentPerson[0], decimal.Parse(currentPerson[1])));
+                }
+                for (int i = 0; i < products.Length; i++)
+                {
+                    string[] currentProduct = products[i].Split("=", StringSplitOptions.RemoveEmptyEntries);
+                    productsList.Add(new Product(currentProduct[0], decimal.Parse(currentProduct[1])));
+                }
+                string command = Console.ReadLine();
+                while (command != "END")
+                {
+                    string[] arguments = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    string name = arguments[0];
+                    string product = arguments[1];
+                    var currentPerson = peopleList.Find(p => p.Name == name);
+                    var currentProduct = productsList.Find(p => p.Name == product);
+                    currentPerson.Shop(currentProduct);
+                    command = Console.ReadLine();
+                }
+                foreach (var person in peopleList)
+                {
+                    if (person.ProductList.Count == 0)
+                    {
+                        Console.WriteLine($"{person.Name} - Nothing bought");
+                    }
+                    else
+                    {
+                        Console.Write($"{person.Name} - ");
+                        Console.WriteLine(string.Join(", ", person.ProductList.Select(p=>p.Name)));
+                    }
+                }
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
