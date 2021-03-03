af7da723a5cfc5e9942a1f6aad756b38524a092a
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-03-03 14:40:20 +0200

    commit

---
+++ Interfaces and Abstraction Exercise/Telephony/Smartphone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class Smartphone : ICallable, IBrowseable
+    {
+        public string Browse(string url)
+        {
+            if (url.Any(char.IsNumber))
+            {
+                return "Invalid URL!";
+            }
+            else
+            {
+                return $"Browsing: {url}!";
+            }
+        }
+
+        public string Call(string number)
+        {
+            if (number.All(char.IsDigit))
+            {
+                return $"Calling... {number}";
+            }
+            else
+            {
+                return "Invalid number!";
+            }
+        }
+    }
+}
---
+++ Interfaces and Abstraction Exercise/Telephony/StationaryPhone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class StationaryPhone : ICallable
+    {
+        public string Call(string number)
+        {
+            if (number.All(char.IsDigit))
+            {
+                return $"Dialing... {number}";
+            }
+            else
+            {
+                return "Invalid number!";
+            }
+        }
+    }
+}
