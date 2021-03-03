e0118baaa24f94d2f0f0d7c901ef7cd3933647aa
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-03-03 19:17:40 +0200

    commit

---
+++ Interfaces and Abstraction Exercise/BorderControl/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BorderControl
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            List<IIdentity> identities = new List<IIdentity>();
+            while (input[0] != "End")
+            {
+                if (input.Length == 2)
+                {
+                    IIdentity robot = new Robot(input[0], input[1]);
+                    identities.Add(robot);
+                }
+                else if(input.Length == 3)
+                {
+                    IIdentity citizen = new Citizen(input[0], int.Parse(input[1]), input[2]);
+                    identities.Add(citizen);
+                }
+
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            string fakeId = Console.ReadLine();
+            foreach (var identity in identities)
+            {
+                if (identity.Id.EndsWith(fakeId))
+                {
+                    Console.WriteLine(identity.Id);
+                }
+            }
+        }
+    }
+}
