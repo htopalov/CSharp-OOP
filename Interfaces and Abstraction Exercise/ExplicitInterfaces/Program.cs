c193c5b4053f571f40f1d2f52ab6872eadf8da13
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-03-04 14:54:10 +0200

    commit

---
+++ Interfaces and Abstraction Exercise/ExplicitInterfaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "End")
+            {
+                IPerson person = new Citizen(input[0], input[1], int.Parse(input[2]));
+                IResident resident = new Citizen(input[0], input[1], int.Parse(input[2]));
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName() + person.GetName());
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
