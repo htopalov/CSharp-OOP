8b630d002d9087669a0635d12b8ab13607f4d7e0
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-03-06 11:20:37 +0200

    adding problems from polymorph lecture

---
+++ Polymorphism Lab/Animals/Animal.cs
@@ -0,0 +1,18 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+
+        public Animal(string name, string food)
+        {
+            this.Name = name;
+            this.Food = food;
+        }
+        public string Name { get; private set; }
+        public string Food { get; private set; }
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.Food}";
+        }
+    }
+}
