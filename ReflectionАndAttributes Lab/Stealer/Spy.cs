f22c1265ce3fde001ef30beda9629b93a362332b
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-03-17 12:29:51 +0200

    commit

---
+++ ReflectionАndAttributes Lab/Stealer/Spy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, string[] fieldNames)
+        {
+            StringBuilder sb = new StringBuilder();
+            Type classType = Type.GetType(className);
+            sb.AppendLine($"Class under investigation: {classType.FullName}");
+            FieldInfo[] fields = classType.GetFields(BindingFlags.Public |
+                BindingFlags.Instance |
+                BindingFlags.NonPublic)
+                .Where(x => fieldNames.Contains(x.Name))
+                .ToArray();
+
+            Object instance = Activator.CreateInstance(classType, new object[] { });
+
+            foreach (var field in fields)
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+        public string AnalyzeAccessModifiers(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type classType = Type.GetType(className);
+            FieldInfo[] fields = classType.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            MethodInfo[] publicMethods = classType.GetMethods(BindingFlags.Public |
+                                         BindingFlags.Instance)
+                                         .Where(n => n.Name.StartsWith("set"))
+                                         .ToArray();
+
+            MethodInfo[] privateMethods = classType.GetMethods(BindingFlags.NonPublic)
+                                          .Where(n => n.Name.StartsWith("get"))
+                                          .ToArray();
+            foreach (var field in fields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+
+            foreach (var method in publicMethods)
+            {
+                sb.AppendLine($"{method.Name} have to be private!");
+            }
+     
+            foreach (var method in privateMethods)
+            {
+                sb.AppendLine($"{method.Name} have to be public!");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ ReflectionАndAttributes Lab/Stealer/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string resultFields = spy.StealFieldInfo("Stealer.Hacker", new string[] { "username", "password" });
+            Console.WriteLine(resultFields);
+
+            string resultModifiers = spy.AnalyzeAccessModifiers("Hacker");
+            Console.WriteLine(resultModifiers);
+        }
+    }
+}
