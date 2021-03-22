fba1b48c327872f087ce501108afa8f6318fe081
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-03-22 20:12:38 +0200

    refactor some elements

--- Unit Tests Exercise/CarManager.Tests/CarTests.cs
+++ Unit Tests Exercise/CarManager.Tests/CarTests.cs
@@ -10,13 +10,7 @@ namespace Tests
         private const string make = "Subaru";
         private const string model = "Impreza";
         private const double fuelConsumption = 10.0;
-        private const double negativeFuelConsumption = -2.0;
-        private const double zeroFuelConsumption = 0;
         private const double fuelCapacity = 50.0;
-        private const double negativeFuelCapacity = -2.0;
-        private const double zeroFuelCapacity = 0;
-        private const double negativeFuelToRefuel = -3.0;
-        private const double zeroFuelToRefuel = 0;
         private const double distance = 10000.0;
 
         [SetUp]
@@ -29,7 +23,6 @@ namespace Tests
         public void Test_If_Constructors_Sets_Data_Correct()
         {
             Assert.That(car.FuelAmount, Is.EqualTo(0));
-
             Assert.That(make, Is.EqualTo(car.Make));
             Assert.That(model, Is.EqualTo(car.Model));
             Assert.That(fuelConsumption, Is.EqualTo(car.FuelConsumption));
@@ -43,7 +36,6 @@ namespace Tests
             {
                 Car car = new Car(string.Empty, model, fuelConsumption, fuelCapacity);
             });
-            Assert.That(ex.Message, Is.EqualTo("Make cannot be null or empty!"));
         }
         [Test]
         public void Property_Model_Setting_Correct_Value()
@@ -53,39 +45,34 @@ namespace Tests
             {
                 Car car = new Car(make, string.Empty, fuelConsumption, fuelCapacity);
             });
-            Assert.That(ex.Message, Is.EqualTo("Model cannot be null or empty!"));
         }
-        [Test]
-        public void Property_FuelConsumption_Setting_Correct_Value()
+        [TestCase(0)]
+        [TestCase(-10)]
+        public void Property_FuelConsumption_Setting_Correct_Value(double consumption)
         {
 
             var ex = Assert.Throws<ArgumentException>(() =>
             {
-                Car car = new Car(make, model, negativeFuelConsumption, fuelCapacity);
-                Car car2 = new Car(make, model, zeroFuelConsumption, fuelCapacity);
+                var car = new Car(make, model, consumption, fuelCapacity);
             });
-            Assert.That(ex.Message, Is.EqualTo("Fuel consumption cannot be zero or negative!"));
         }
-        [Test]
-        public void Property_FuelCapacity_Setting_Correct_Value()
+        [TestCase(0)]
+        [TestCase(-10)]
+        public void Property_FuelCapacity_Setting_Correct_Value(double capacity)
         {
             var ex = Assert.Throws<ArgumentException>(() =>
             {
-                Car car = new Car(make, model, fuelConsumption, negativeFuelCapacity);
-                Car car2 = new Car(make, model, fuelConsumption, zeroFuelCapacity);
+                var car = new Car(make, model, fuelConsumption, capacity);
             });
-            Assert.That(ex.Message, Is.EqualTo("Fuel capacity cannot be zero or negative!"));
         }
-        [Test]
-        public void Refuel_Throws_Exception_If_Refuel_Fuel_Is_Negative_Or_Zero()
+        [TestCase(0)]
+        [TestCase(-10)]
+        public void Refuel_Throws_Exception_If_Refuel_Fuel_Is_Negative_Or_Zero(double fuel)
         {
             var ex = Assert.Throws<ArgumentException>(() =>
             {
-                car.Refuel(negativeFuelToRefuel);
-                Car car2 = new Car(make, model, fuelConsumption, fuelCapacity);
-                car2.Refuel(zeroFuelToRefuel);
+                car.Refuel(fuel);;
             });
-            Assert.That(ex.Message, Is.EqualTo("Fuel amount cannot be zero or negative!"));
         }
         [Test]
         public void Drive_Throws_Exception_For_Not_Sufficient_Fuel_To_Drive()
@@ -94,7 +81,6 @@ namespace Tests
             {
                 car.Drive(distance);
             });
-            Assert.That(ex.Message, Is.EqualTo("You don't have enough fuel to drive!"));
         }
         [Test]
         public void Test_If_Refuel_More_Than_Capacity()
