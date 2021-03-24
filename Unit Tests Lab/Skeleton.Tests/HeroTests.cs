43b543fa55a870287c1362f5447195addd5f57c9
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-03-24 08:39:36 +0200

    commit refactoring for mocking objects

---
+++ Unit Tests Lab/Skeleton.Tests/HeroTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using NUnit.Framework;
+using Skeleton.Contracts;
+
+[TestFixture]
+public class HeroTests
+{
+    [Test]
+    public void Target_Gives_Experiance_When_Dead()
+    {
+        Mock<IWeapon> mockWeapon = new Mock<IWeapon>();
+        Mock<ITarget> mockTarget = new Mock<ITarget>();
+        mockTarget.Setup(d => d.IsDead()).Returns(true);
+        mockTarget.Setup(e => e.GiveExperience()).Returns(100);
+        mockTarget.Setup(h => h.Health).Returns(0);
+        Hero hero = new Hero("Ivan", mockWeapon.Object);
+        hero.Attack(mockTarget.Object);
+        Assert.AreEqual(hero.Experience, 100);
+        
+    }
+}
---
+++ Unit Tests Lab/Skeleton/Contracts/ITarget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skeleton.Contracts
+{
+    public interface ITarget
+    {
+        int Health { get; }
+        void TakeAttack(int attack);
+        int GiveExperience();
+        bool IsDead();
+
+    }
+}
