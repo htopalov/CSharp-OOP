8e2d28bdb8451230a8fed7e055d0023d1f16708b
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-03-27 12:51:37 +0200

    commit

---
+++ Design Patterns/Composite/CompositeGift.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    public class CompositeGift : GiftBase, IGiftOperations
+    {
+        private List<GiftBase> gifts;
+
+        public CompositeGift(string name, int price) 
+            : base(name, price)
+        {
+            gifts = new List<GiftBase>();
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            int totalPrice = 0;
+            Console.WriteLine($"{this.name} contains the following products with prices:");
+            foreach (var gift in gifts)
+            {
+                totalPrice += gift.CalculateTotalPrice();
+            }
+            return totalPrice;
+        }
+        public void Add(GiftBase gift)
+        {
+            gifts.Add(gift);
+        }
+        public void Remove(GiftBase gift)
+        {
+            gifts.Remove(gift);
+        }
+    }
+}
---
+++ Design Patterns/Composite/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phone = new SingleGift("Samsung", 500);
+            phone.CalculateTotalPrice();
+            Console.WriteLine();
+
+            var bigBox = new CompositeGift("Big box", 0);
+            var toy1 = new SingleGift("Car", 10);
+            var toy2 = new SingleGift("Bear", 12);
+            bigBox.Add(toy1);
+            bigBox.Add(toy2);
+            var smallBox = new CompositeGift("Small box", 0);
+            var toy3 = new SingleGift("playstation", 400);
+            var toy4 = new SingleGift("sword", 11);
+            smallBox.Add(toy3);
+            smallBox.Add(toy4);
+            bigBox.Add(smallBox);
+
+
+            Console.WriteLine($"Total price of composite present is: {bigBox.CalculateTotalPrice()}");
+        }
+
+    }
+}
