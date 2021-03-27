dd3b7d77af4135541e439ca5bb47b1e787a2db00
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-03-27 11:30:59 +0200

    commit

---
+++ Design Patterns/Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDataContainer.Instance;
+            Console.WriteLine(db.GetPopulation("Kazanlak"));
+            var db2 = SingletonDataContainer.Instance;
+            Console.WriteLine(db2.GetPopulation("Sofia"));
+            var db3 = SingletonDataContainer.Instance;
+            Console.WriteLine(db3.GetPopulation("Plovdiv"));
+        }
+    }
+}
---
+++ Design Patterns/Singleton/SingletonDataContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Singleton
+{
+    public class SingletonDataContainer : ISingletonContainer
+    {
+        private Dictionary<string, int> capitals = new Dictionary<string, int>();
+        private SingletonDataContainer()
+        {
+            var elements = File.ReadAllLines("../../../capitals.txt");
+            for (int i = 0; i < elements.Length; i+=2)
+            {
+                capitals.Add(elements[i], int.Parse(elements[i + 1]));
+            }
+        }
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+
+        private static SingletonDataContainer instance = new SingletonDataContainer();
+        public static SingletonDataContainer Instance => instance;
+    }
+}
