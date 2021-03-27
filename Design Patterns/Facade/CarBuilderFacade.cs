c7c1518517e3c0805c949497992cccbfbb1a1370
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-03-27 11:50:45 +0200

    commit

---
+++ Design Patterns/Facade/CarBuilderFacade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    public class CarBuilderFacade
+    {
+        protected Car Car { get; set; }
+        public CarBuilderFacade()
+        {
+            Car = new Car();
+        }
+        public Car Build() => Car;
+        public CarInfoBuilder infoBuilder => new CarInfoBuilder(Car);
+        public CarAddressBuilder addressBuilder => new CarAddressBuilder(Car);
+    }
+}
---
+++ Design Patterns/Facade/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new CarBuilderFacade()
+                .infoBuilder
+                  .WithType("Zaporojec")
+                  .WithColor("Glowing red")
+                  .WithNumbeRofDoors(5)
+                .addressBuilder
+                  .CityOfCar("Sofia")
+                  .AddressOfCar("bul. T. Aleksandrov 1")
+                  .Build();
+            Console.WriteLine(car);
+        }
+    }
+}
