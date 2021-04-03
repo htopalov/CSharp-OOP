631936c3affcef63499eca6fc214bfd01725f778
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-04-03 12:19:39 +0300

    commit problem not finished

---
+++ Exam Preparation/C# OOP Exam - 12 December 2020/Problem 1-2/Bakery/Core/Controller.cs
@@ -0,0 +1,155 @@
+﻿using Bakery.Core.Contracts;
+using Bakery.Models.BakedFoods;
+using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Models.Drinks;
+using Bakery.Models.Drinks.Contracts;
+using Bakery.Models.Tables;
+using Bakery.Models.Tables.Contracts;
+using Bakery.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bakery.Core
+{
+    public class Controller : IController
+    {
+        private List<IBakedFood> bakedFoods;
+        private List<IDrink> drinks;
+        private List<ITable> tables;
+        private decimal totalIncome;
+
+        public Controller()
+        {
+            this.bakedFoods = new List<IBakedFood>();
+            this.drinks = new List<IDrink>();
+            this.tables = new List<ITable>();
+        }
+        public string AddDrink(string type, string name, int portion, string brand)
+        {
+            Drink drink = null;
+            switch (type)
+            {
+                case "Water":
+                    drink = new Water(name, portion, brand);
+                    break;
+                case "Tea":
+                    drink = new Tea(name, portion, brand);
+                    break;
+            }
+            drinks.Add(drink);
+
+            return string.Format(OutputMessages.DrinkAdded, drink.Name, drink.Brand);
+        }
+
+        public string AddFood(string type, string name, decimal price)
+        {
+            BakedFood food = null;
+            switch (type)
+            {
+                case "Bread":
+                    food = new Bread(name, price);
+                    break;
+                case "Cake":
+                    food = new Cake(name, price);
+                    break;
+            }
+            bakedFoods.Add(food);
+
+            return string.Format(OutputMessages.FoodAdded, food.Name, food.GetType().Name);
+        }
+
+        public string AddTable(string type, int tableNumber, int capacity)
+        {
+            Table table = null;
+            switch (type)
+            {
+                case "InsideTable":
+                    table = new InsideTable(tableNumber, capacity);
+                    break;
+                case "OutsideTable":
+                    table = new OutsideTable(tableNumber, capacity);
+                    break;
+            }
+            tables.Add(table);
+
+            return string.Format(OutputMessages.TableAdded, table.TableNumber);
+        }
+
+        public string GetFreeTablesInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            var freeTables = tables.Where(x => x.IsReserved == false);
+            foreach (var table in freeTables)
+            {
+               sb.AppendLine(table.GetFreeTableInfo());
+            }
+            return sb.ToString().Trim();
+        }
+
+        public string GetTotalIncome()
+        {
+            return string.Format(OutputMessages.TotalIncome, totalIncome);
+        }
+
+        public string LeaveTable(int tableNumber)
+        {
+            var table = tables.FirstOrDefault(x => x.TableNumber == tableNumber);
+            decimal bill = table.GetBill();
+            totalIncome += bill;
+            table.Clear();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Table: {tableNumber}");
+            sb.AppendLine($"Bill: {bill:f2}");
+            return sb.ToString().Trim();
+        }
+
+        public string OrderDrink(int tableNumber, string drinkName, string drinkBrand)
+        {
+            var table = tables.FirstOrDefault(x => x.TableNumber == tableNumber);
+            var drink = drinks.FirstOrDefault(x => x.Name == drinkName && x.Brand == drinkBrand);
+            if (table == null)
+            {
+                return string.Format(OutputMessages.WrongTableNumber, tableNumber);
+            }
+            if (drink == null)
+            {
+                return string.Format(OutputMessages.NonExistentDrink, drinkName, drinkBrand);
+            }
+            table.OrderDrink(drink);
+            return $"Table {tableNumber} ordered {drinkName} {drinkBrand}";
+        }
+
+        public string OrderFood(int tableNumber, string foodName)
+        {
+            var table = tables.FirstOrDefault(x => x.TableNumber == tableNumber);
+            var food = bakedFoods.FirstOrDefault(x => x.Name == foodName);
+            if (table == null)
+            {
+                return string.Format(OutputMessages.WrongTableNumber, tableNumber);
+            }
+            if (food == null)
+            {
+                return string.Format(OutputMessages.NonExistentFood, foodName);
+            }
+            table.OrderFood(food);
+            return string.Format(OutputMessages.FoodOrderSuccessful, tableNumber, foodName);
+
+        }
+
+        public string ReserveTable(int numberOfPeople)
+        {
+            var table = tables.FirstOrDefault(x => x.IsReserved == false && x.Capacity >= numberOfPeople);
+            if (table == null)
+            {
+                return string.Format(OutputMessages.ReservationNotPossible, numberOfPeople);
+            }
+            else
+            {
+                table.Reserve(numberOfPeople);
+                return string.Format(OutputMessages.TableReserved, table.TableNumber, numberOfPeople);
+            }
+        }
+    }
+}
---
+++ Exam Preparation/C# OOP Exam - 12 December 2020/Problem 1-2/Bakery/Models/Tables/Table.cs
@@ -0,0 +1,117 @@
+﻿using Bakery.Models.BakedFoods;
+using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Models.Drinks.Contracts;
+using Bakery.Models.Tables.Contracts;
+using Bakery.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bakery.Models.Tables
+{
+    public abstract class Table : ITable
+    {
+        private List<IBakedFood> FoodOrders;
+        private List<IDrink> DrinkOrders;
+        private int capacity;
+        private int numberOfPeople;
+
+        public Table(int tableNumber, int capacity, decimal pricePerPerson)
+        {
+            this.FoodOrders = new List<IBakedFood>();
+            this.DrinkOrders = new List<IDrink>();
+            this.TableNumber = tableNumber;
+            this.Capacity = capacity;
+            this.PricePerPerson = pricePerPerson;
+        }
+
+        public int TableNumber { get; private set; }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.capacity;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidTableCapacity);
+                }
+                this.capacity = value;
+            }
+        }
+
+        public int NumberOfPeople
+        {
+            get
+            {
+                return this.numberOfPeople;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidNumberOfPeople);
+                }
+                this.numberOfPeople = value;
+            }
+        }
+
+        public decimal PricePerPerson { get; private set; }
+
+        public bool IsReserved { get; private set; }
+
+        public decimal Price
+        {
+            get
+            {
+                return PricePerPerson * NumberOfPeople;
+            }
+        }
+
+        public void Clear()
+        {
+            DrinkOrders.Clear();
+            FoodOrders.Clear();
+            this.numberOfPeople = 0; //does it throw exception??????
+            IsReserved = false;
+        }
+
+        public decimal GetBill()
+        {
+            decimal drinksTotalPrice = DrinkOrders.Sum(x => x.Price);
+            decimal foodTotalPrice = FoodOrders.Sum(x => x.Price);
+            decimal total = drinksTotalPrice + foodTotalPrice + Price;
+            return total;
+        }
+
+        public string GetFreeTableInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Table: {this.TableNumber}");
+            sb.AppendLine($"Type: {this.GetType().Name}");
+            sb.AppendLine($"Capacity: {this.Capacity}");
+            sb.AppendLine($"Price per Person: {this.PricePerPerson:F2}");
+            return sb.ToString().Trim();
+        }
+
+        public void OrderDrink(IDrink drink)
+        {
+            DrinkOrders.Add(drink);
+        }
+
+        public void OrderFood(IBakedFood food)
+        {
+            FoodOrders.Add(food);
+        }
+
+        public void Reserve(int numberOfPeople)
+        {
+            this.NumberOfPeople = numberOfPeople;
+            IsReserved = true;
+        }
+    }
+}
