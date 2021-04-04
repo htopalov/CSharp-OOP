30f4d99888c48d6f634a4ceffccb4ab9a823e7d8
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-04-04 14:13:21 +0300

    commit exam 285/300  - 2 test not passing in problem 2

---
+++ Exam Preparation/C# OOP Exam - 16 August 2020/Problem 1-2/OnlineShop/Core/Controller.cs
@@ -0,0 +1,184 @@
+﻿using OnlineShop.Common.Constants;
+using OnlineShop.Models.Products.Components;
+using OnlineShop.Models.Products.Computers;
+using OnlineShop.Models.Products.Peripherals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineShop.Core
+{
+    public class Controller : IController
+    {
+        private List<IComputer> computers;
+        private List<IComponent> components;
+        private List<IPeripheral> peripherals;
+
+        public Controller()
+        {
+            this.components = new List<IComponent>();
+            this.computers = new List<IComputer>();
+            this.peripherals = new List<IPeripheral>();
+        }
+        public string AddComponent(int computerId, int id, string componentType, string manufacturer, string model, decimal price, double overallPerformance, int generation)
+        {
+            var computer = CheckComputersId(computerId);
+            IComponent component = null;
+            switch(componentType)
+            {
+                case "CentralProcessingUnit":
+                    component = new CentralProcessingUnit(id, manufacturer, model, price, overallPerformance, generation);
+                    break;
+                case "Motherboard":
+                    component = new Motherboard(id, manufacturer, model, price, overallPerformance, generation);
+                    break;
+                case "PowerSupply":
+                    component = new PowerSupply(id, manufacturer, model, price, overallPerformance, generation);
+                    break;
+                case "RandomAccessMemory":
+                    component = new RandomAccessMemory(id, manufacturer, model, price, overallPerformance, generation);
+                    break;
+                case "SolidStateDrive":
+                    component = new SolidStateDrive(id, manufacturer, model, price, overallPerformance, generation);
+                    break;
+                case "VideoCard":
+                    component = new VideoCard(id, manufacturer, model, price, overallPerformance, generation);
+                    break;
+                default:
+                    throw new ArgumentException(ExceptionMessages.InvalidComponentType);
+            }
+            if (components.Any(x=>x.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.ExistingComponentId);
+            }
+           
+            computer.AddComponent(component);
+            components.Add(component);   
+
+            return string.Format(SuccessMessages.AddedComponent, componentType, id, computerId);
+
+        }
+
+        public string AddComputer(string computerType, int id, string manufacturer, string model, decimal price)
+        {
+            IComputer computer = null;
+
+            switch(computerType)
+            {
+                case "Laptop":
+                    computer = new Laptop(id, manufacturer, model, price);
+                    break;
+                case "DesktopComputer":
+                    computer = new DesktopComputer(id, manufacturer, model, price);
+                    break;
+                default:
+                    throw new ArgumentException(ExceptionMessages.InvalidComputerType);
+            }
+
+            if (computers.Any(x=>x.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.ExistingComputerId);
+            }
+
+            computers.Add(computer);
+
+            return string.Format(SuccessMessages.AddedComputer, id);
+        }
+
+        public string AddPeripheral(int computerId, int id, string peripheralType, string manufacturer, string model, decimal price, double overallPerformance, string connectionType)
+        {
+            var computer = CheckComputersId(computerId);
+            IPeripheral peripheral = null;
+            switch(peripheralType)
+            {
+                case "Headset":
+                    peripheral = new Headset(id, manufacturer, model, price, overallPerformance, connectionType);
+                    break;
+                case "Keyboard":
+                    peripheral = new Keyboard(id, manufacturer, model, price, overallPerformance, connectionType);
+                    break;
+                case "Monitor":
+                    peripheral = new Monitor(id, manufacturer, model, price, overallPerformance, connectionType);
+                    break;
+                case "Mouse":
+                    peripheral = new Mouse(id, manufacturer, model, price, overallPerformance, connectionType);
+                    break;
+                default:
+                    throw new ArgumentException(ExceptionMessages.InvalidPeripheralType);
+            }
+            if (peripherals.Any(x => x.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.ExistingPeripheralId);
+            }
+
+            computer.AddPeripheral(peripheral);
+            peripherals.Add(peripheral);
+
+            return string.Format(SuccessMessages.AddedPeripheral, peripheralType, id, computer.Id);
+        }
+
+        public string BuyBest(decimal budget)
+        {
+            var computer = computers
+                              .Where(x => x.Price <= budget)
+                              .OrderByDescending(x => x.OverallPerformance)
+                              .FirstOrDefault();
+            if (computer == null)
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.CanNotBuyComputer, budget));
+            }
+
+            computers.Remove(computer);
+
+            return computer.ToString();
+
+        }
+
+        public string BuyComputer(int id)
+        {
+            var computer = CheckComputersId(id);
+            string result = computer.ToString();
+            computers.Remove(computer);
+
+            return result;
+        }
+
+        public string GetComputerData(int id)
+        {
+            var computer = CheckComputersId(id);
+
+            return computer.ToString();
+        }
+
+        public string RemoveComponent(string componentType, int computerId)
+        {
+            var computer = CheckComputersId(computerId);
+            var component = components.FirstOrDefault(x => x.GetType().Name == componentType);
+            computer.RemoveComponent(componentType);
+            components.Remove(component);
+
+            return string.Format(SuccessMessages.RemovedComponent, componentType, component.Id);
+        }
+
+        public string RemovePeripheral(string peripheralType, int computerId)
+        {
+            var computer = CheckComputersId(computerId);
+            computer.RemovePeripheral(peripheralType);
+            var peripheral = peripherals.FirstOrDefault(x => x.GetType().Name == peripheralType);
+            peripherals.Remove(peripheral);
+
+            return string.Format(SuccessMessages.RemovedPeripheral, peripheralType, peripheral.Id);
+        }
+
+        private IComputer CheckComputersId(int id)
+        {
+            var computer = computers.FirstOrDefault(x => x.Id == id);
+            if (computer == null)
+            {
+                throw new ArgumentException(ExceptionMessages.NotExistingComputerId);
+            }
+            return computer;
+        }
+    }
+}
---
+++ Exam Preparation/C# OOP Exam - 16 August 2020/Problem 1-2/OnlineShop/Models/Products/Computers/Computer.cs
@@ -0,0 +1,115 @@
+﻿using OnlineShop.Common.Constants;
+using OnlineShop.Models.Products.Components;
+using OnlineShop.Models.Products.Peripherals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineShop.Models.Products.Computers
+{
+    public abstract class Computer : Product, IComputer
+    {
+        private List<IComponent> components;
+        private List<IPeripheral> peripherals;
+
+        protected Computer(int id, string manufacturer, string model, decimal price, double overallPerformance)
+            : base(id, manufacturer, model, price, overallPerformance)
+        {
+            this.components = new List<IComponent>();
+            this.peripherals = new List<IPeripheral>();
+        }
+
+        public IReadOnlyCollection<IComponent> Components => components;
+
+        public IReadOnlyCollection<IPeripheral> Peripherals => peripherals;
+
+        public override decimal Price
+        {
+            get
+            {
+                return base.Price + components.Sum(x => x.Price) + peripherals.Sum(x => x.Price);
+            }
+        }
+
+        public override double OverallPerformance 
+        { 
+            get
+            {
+                if (components.Count == 0)
+                {
+                    return base.OverallPerformance;
+                }
+                else
+                {
+                    return base.OverallPerformance + components.Average(x => x.OverallPerformance);
+                }
+            }
+        }
+
+        public void AddComponent(IComponent component)
+        {
+            if (components.Contains(component))
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.ExistingComponent, component.GetType().Name, this.GetType().Name.Length, Id));
+            }
+            components.Add(component);
+        }
+
+        public void AddPeripheral(IPeripheral peripheral)
+        {
+            if (peripherals.Contains(peripheral))
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.ExistingPeripheral, peripheral.GetType().Name, this.GetType().Name.Length, Id));
+            }
+            peripherals.Add(peripheral);
+        }
+
+        public IComponent RemoveComponent(string componentType)
+        {
+            if (components.Count == 0 || !components.Any(x=>x.GetType().Name == componentType))
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.NotExistingComponent, componentType, this.GetType().Name, Id));
+            }
+            var component = components.FirstOrDefault(x => x.GetType().Name == componentType);
+            components.Remove(component);
+            return component;
+        }
+
+        public IPeripheral RemovePeripheral(string peripheralType)
+        {
+            if (peripherals.Count == 0 || peripherals.Any(x => x.GetType().Name != peripheralType))
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.NotExistingPeripheral, peripheralType, this.GetType().Name, Id));
+            }
+            var peripheral = peripherals.FirstOrDefault(x => x.GetType().Name == peripheralType);
+            peripherals.Remove(peripheral);
+            return peripheral;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ToString());
+            sb.AppendLine($" Components ({components.Count}):");
+            foreach (var component in components)
+            {
+                sb.AppendLine("  " + component.ToString());
+            }
+            if (peripherals.Count > 0)
+            {
+                sb.AppendLine($" Peripherals ({peripherals.Count}); Average Overall Performance ({peripherals.Average(x => x.OverallPerformance):f2}):");
+                foreach (var peripheral in peripherals)
+                {
+                    sb.AppendLine("  " + peripheral.ToString());
+                }
+            }
+            else
+            {
+                sb.AppendLine(" Peripherals (0); Average Overall Performance (0.00):");
+            }
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
