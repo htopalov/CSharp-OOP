4a05bb900c7de5d14e8f760b552e7616c8118392
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-04-06 20:26:23 +0300

    commit unit test from exam

---
+++ Exam Preparation/C# OOP Exam - 12 April 2020/Problem 3/Robots.Tests/RobotsTests.cs
@@ -0,0 +1,111 @@
+﻿namespace Robots.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    public class RobotsTests
+    {
+        private RobotManager manager;
+        private Robot robot;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.manager = new RobotManager(5);
+            this.robot = new Robot("Name", 100);
+        }
+        [Test]
+        public void CheckIfConstructorWorks()
+        {
+            Assert.IsNotNull(manager);
+        }
+        [Test]
+        public void CheckIfCapacityPropertyThrowsExceptionWhenIsLessThanZero()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                RobotManager rmanager = new RobotManager(-10);
+            });
+        }
+        [Test]
+        public void CheckIfAddCommandWorksCorrectly()
+        {
+            manager.Add(robot);
+            Assert.AreEqual(manager.Count, 1);
+        }
+        [Test]
+        public void CheckIfAddCommandThrowsExceptionWhenRobotExists()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                manager.Add(robot);
+                manager.Add(robot);
+            });
+        }
+        [Test]
+        public void CheckIfAddCommandThrowsExceptionWhenCapacityNotEnough()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    manager.Add(new Robot($"robot {i}", 100 + i));
+                }
+            });
+        }
+        [Test]
+        public void CheckIfRemoveRobotMethodWorksCorrectly()
+        {
+            manager.Add(robot);
+            manager.Remove("Name");
+            Assert.AreEqual(manager.Count, 0);
+        }
+        [Test]
+        public void CheckIfRemoveRobotMethodThrowsExceptionWhenRobotNotExisting()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                manager.Remove("Terminator");
+            });
+        }
+        [Test]
+        public void CheckIfWorkMethodThrowsExceptionWhenRobotNotExisting()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                manager.Work("T-100", "Dig", 15);
+            });
+        }
+        [Test]
+        public void CheckIfWorkMethodThrowsExceptionWhenRobotBatteryNotEnoughToWork()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                manager.Work("Name", "Dig", 150);
+            });
+        }
+        [Test]
+        public void CheckIfWorkMethodWorksCorrectly()
+        {
+            manager.Add(robot);
+            manager.Work("Name", "Dig", 15);
+            Assert.AreEqual(robot.Battery, 85);
+        }
+        [Test]
+        public void CheckIfChargeMethodThrowsExceptionWhenRobotMissing()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                manager.Charge("T1000");
+            });
+        }
+        [Test]
+        public void CheckIfChargeMethodWorksCorrectly()
+        {
+            manager.Add(robot);
+            manager.Work("Name", "Dig", 50);
+            manager.Charge("Name");
+            Assert.AreEqual(robot.Battery, 100);
+        }
+    }
+}
